d7499d5549b14c346c4bbd40baceac6d727be88b
EdOphanimRawr <erp.reno@hotmail.com>
2025-03-23 19:30:04 -0400

    added card pkg

--- app/src/main/java/com/example/myapplication/Cards/ExpandableCard.kt
+++ app/src/main/java/com/example/myapplication/Cards/ExpandableCard.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.myapplication.Cards
 
 
 import androidx.compose.animation.animateContentSize
@@ -7,15 +7,12 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
